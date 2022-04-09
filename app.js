ac378fe10f219fb7220aa9b16d1e0c3419a7000a
Ruchi <ruchi.thanabhawan@gmail.com>
2022-04-09 22:38:11 +0530

    post create read delete

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const yup = require('yup')
 const User = require('./models/User')
 const bcrypt = require('bcrypt')
 const { compareSync } = require('bcrypt')
+const Post = require('./models/Posts')
+const req = require('express/lib/request')
 require('dotenv').config()
 
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI)
-.catch (function(e) {
-   console.log('failed to connect to database', e)
-})  
+  .catch(function (e) {
+    console.log('failed to connect to database', e)
+  })
 app.use(express.json())
 
 app.post('/sign-up', async (req, res) => {
@@ -55,11 +57,45 @@ app.post('/log-in', async (req, res) => {
   const user = await User.findOne({ email: req.body.email }).exec()
 
   if (!user) return res.status(404).json({ error: "User not found" })
-  
+
 
   if (!bcrypt.compareSync(req.body.password, user.password)) return res.status(404).json({ error: "Incorrect password" })
 
   return res.json(user)
+})
+
+// Blog Post CRUD
+
+app.post('/posts', async (req, res) => {
+  try {
+
+    await yup.object({
+      title: yup.string().required(),
+      userId: yup.string().required(),
+      body: yup.string().required()
+    })
+      .validate(req.body)
+  } catch (e) {
+    return res.json(e.errors)
+  }
+
+  const post = await Post.create({...req.body, slug: req.body.title.toLowerCase().replaceAll(' ', '-')})
+
+  return res.status(201).json(post)
+
+})
+
+app.get('/posts', async (req, res) => {
+  return res.json(await Post.find().exec())
+})
+
+app.get('/posts/:slug', async (req, res) => {
+  return res.json(await Post.findOne({ slug: req.params.slug}).exec())
+})
+
+app.delete('/posts/:id', async (req, res) => {
+  await Post.findByIdAndDelete( req.params.id).exec()
+  return res.status(204).send()
 })
 
 app.listen(8000, function () {
---
+++ models/Posts.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  title: { type: String, required: true, unique: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  body: { type: String, required: true },
+  slug: { type: String, required: true, unique: true }
+});
+
+const Post = mongoose.model('Post', schema);
+
+module.exports = Post
