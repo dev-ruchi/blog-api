d00a71fd434ec943e01fe69f57b617e0211f774b
Ruchi <ruchi.thanabhawan@gmail.com>
2022-04-09 17:08:00 +0530

    add endpoint for login

--- app.js
+++ app.js
@@ -21,6 +21,20 @@ app.post('/sign-up', async (req, res) => {
   return res.json(req.body)
 })
 
+app.post('/log-in', async (req, res) => {
+  try {
+    await yup.object({
+      email: yup.string().email(),
+      password: yup.string().required().min(6)
+    })
+      .validate(req.body)
+  } catch (e) {
+    return res.json(e.errors)
+  }
+
+  return res.json(req.body)
+})
+
 app.listen(8000, function () {
   console.log('Listening on port 8000');
 })
